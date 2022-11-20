4c0abdd20f4742d2325c3ed8858f96cde2456f71
Moritz Mistol <moritz.mistol@gmail.com>
2022-11-20 17:51:19 +0100

    modify DimensionalityReducer
    The class now evaluates the dr properly and also offers a method to
    estimate the instrinsic dimensionality of a dataset

--- drcomp/dimensionality_reducer.py
+++ drcomp/dimensionality_reducer.py
@@ -2,8 +2,10 @@
 
 from abc import ABCMeta, abstractmethod
 
-import numpy as np
+from pyDRMetrics import DRMetrics
+from skdim.id import MLE
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_is_fitted
 
 
 class DimensionalityReducer(BaseEstimator, TransformerMixin, metaclass=ABCMeta):
@@ -23,5 +25,10 @@ class DimensionalityReducer(BaseEstimator, TransformerMixin, metaclass=ABCMeta):
 
     def evaluate(self, X) -> dict:
         """Evaluate the quality of the Dimensionality Reduction."""
-        # TODO: Search for metrics to evaluate the quality of the dimensionality reduction
-        return {"random_number": np.random.random()}
+        check_is_fitted(self)
+        drm = DRMetrics(X, self.transform(X))
+        return {"trustworthiness": drm.T, "continuity": drm.C, "LCMC": drm.LCMC}
+
+    def _estimate_intrinsic_dimension(self, X) -> int:
+        """Estimate the intrinsic dimensionality of the data."""
+        return MLE().fit_transform(X)
