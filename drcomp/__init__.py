a8d182504133b82e0bbcab3241228051f0618989
Moritz Mistol <moritz.mistol@gmail.com>
2022-11-25 11:20:53 +0100

    update drcomp

--- drcomp/__init__.py
+++ drcomp/__init__.py
@@ -1,5 +1,5 @@
 """Dimensionality Reduction Comparison."""
 
-from .dimensionality_reducer import DimensionalityReducer
+from .dimensionality_reducer import DimensionalityReducer, estimate_intrinsic_dimension
 
-__all__ = ["DimensionalityReducer"]
+__all__ = ["DimensionalityReducer", "estimate_intrinsic_dimension"]
--- drcomp/dimensionality_reducer.py
+++ drcomp/dimensionality_reducer.py
@@ -2,10 +2,16 @@
 
 from abc import ABCMeta, abstractmethod
 
-# from pyDRMetrics.pyDRMetrics import DRMetrics
+import pyDRMetrics.pyDRMetrics as metrics
 from skdim.id import MLE
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_is_fitted
+
+
+def estimate_intrinsic_dimension(X) -> int:
+    """Estimate the intrinsic dimensionality of the data."""
+    dimension = MLE().fit_transform(X)
+    return int(dimension)
 
 
 class DimensionalityReducer(BaseEstimator, TransformerMixin, metaclass=ABCMeta):
@@ -26,9 +32,5 @@ class DimensionalityReducer(BaseEstimator, TransformerMixin, metaclass=ABCMeta):
     def evaluate(self, X) -> dict:
         """Evaluate the quality of the Dimensionality Reduction."""
         check_is_fitted(self)
-        # drm = DRMetrics(X, self.transform(X))
-        # return {"trustworthiness": drm.T, "continuity": drm.C, "LCMC": drm.LCMC}
-
-    def _estimate_intrinsic_dimension(self, X) -> int:
-        """Estimate the intrinsic dimensionality of the data."""
-        return MLE().fit_transform(X)
+        drm = metrics.DRMetrics(X, self.transform(X))
+        return {"trustworthiness": drm.T, "continuity": drm.C, "LCMC": drm.LCMC}
