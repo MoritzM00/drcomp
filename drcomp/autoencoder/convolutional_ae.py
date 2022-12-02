dc7c224bc85d2ed5d62d3e2b4d554e21bd15e124
Moritz Mistol <moritz.mistol@gmail.com>
2022-12-02 13:19:25 +0100

    add class for convolutional autoencoder

--- drcomp/autoencoder/convolutional_ae.py
+++ drcomp/autoencoder/convolutional_ae.py
@@ -1,5 +1,11 @@
 import torch.nn as nn
 
 
-class ConvolutionalAE(nn.Module):
-    pass
+class MnistConvolutionalAE(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        encoded = self.encoder.forward(x)
+        decoded = self.decoder.forward(encoded)
+        return decoded, encoded
