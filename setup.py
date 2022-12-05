471a05401b41a2083378bc8bf6757a03ccd8eafb
Moritz Mistol <moritz.mistol@gmail.com>
2022-12-05 17:58:55 +0100

    add install_requires

--- setup.py
+++ setup.py
@@ -7,4 +7,12 @@ setup(
     author_email="moritz.mistol@gmail.com",
     description="Python package for the comparison of dimensionality reduction techniques",
     packages=find_packages(include=["drcomp", "drcomp.*"]),
+    install_requires=[
+        "numpy",
+        "scikit-learn",
+        "torch",
+        "torchvision",
+        "scikit-dimension",
+        "skorch",
+    ],
 )
