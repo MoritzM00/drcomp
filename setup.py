091fbb18bfc44b2b468f11c309a9d62493e32778
Moritz Mistol <moritz.mistol@gmail.com>
2022-12-05 14:32:52 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="drcomp",
-    version="0.0.0",
+    version="0.1.0",
     author="Moritz Mistol",
     author_email="moritz.mistol@gmail.com",
     description="Python package for the comparison of dimensionality reduction techniques",
