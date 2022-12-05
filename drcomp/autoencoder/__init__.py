ae4667076152f9c461ab88f6a097e89f986ab104
Moritz Mistol <moritz.mistol@gmail.com>
2022-12-05 17:59:06 +0100

    remove contractive loss import

--- drcomp/autoencoder/__init__.py
+++ drcomp/autoencoder/__init__.py
@@ -1,13 +1,11 @@
 """This Module provides Classes for the Autoencoders Reducer class."""
 
 from .base import AbstractAutoEncoder
-from .contractive_loss import ContractiveLoss
 from .convolutional_ae import Cifar10ConvAE, MnistConvAE
 from .fully_connected_ae import FullyConnectedAE
 
 __all__ = [
     "AbstractAutoEncoder",
-    "ContractiveLoss",
     "FullyConnectedAE",
     "MnistConvAE",
     "Cifar10ConvAE",
