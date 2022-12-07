0dc8b6a84830fac76cae6a0564789db2eb639c21
Moritz Mistol <moritz.mistol@gmail.com>
2022-12-07 09:24:33 +0100

    allow fully connected ae with different activations

--- drcomp/autoencoder/fully_connected_ae.py
+++ drcomp/autoencoder/fully_connected_ae.py
@@ -1,4 +1,6 @@
 """Fully Connected Autoencoder"""
+from typing import Union
+
 import torch.nn as nn
 
 from drcomp.autoencoder.base import AbstractAutoEncoder
@@ -10,7 +12,7 @@ class FullyConnectedAE(AbstractAutoEncoder):
         input_size: int,
         intrinsic_dim: int,
         hidden_layer_dims: list[int] = [],
-        act_fn: object = nn.Sigmoid,
+        act_fn: Union[object, list[object]] = nn.Sigmoid,
         include_batch_norm: bool = False,
     ):
         super().__init__(intrinsic_dim=intrinsic_dim)
@@ -18,6 +20,12 @@ class FullyConnectedAE(AbstractAutoEncoder):
         self.hidden_layer_dims = hidden_layer_dims
         self.act_fn = act_fn
         self.include_batch_norm = include_batch_norm
+        if not isinstance(act_fn, list):
+            act_fn = [act_fn] * (len(hidden_layer_dims) + 1)
+            print(act_fn)
+        assert (
+            len(act_fn) == len(hidden_layer_dims) + 1
+        ), "act_fn must be a list of length len(hidden_layer_dims) + 1"
 
         # build encoder and decoder
         layer_dims = [input_size, *hidden_layer_dims, intrinsic_dim]
@@ -28,11 +36,11 @@ class FullyConnectedAE(AbstractAutoEncoder):
             encoder.append(nn.Linear(layer_dims[i], layer_dims[i + 1]))
             if include_batch_norm:
                 encoder.append(nn.BatchNorm1d(layer_dims[i + 1]))
-            encoder.append(act_fn())
+            encoder.append(act_fn[i]())
 
             decoder.append(nn.Linear(layer_dims[depth - i], layer_dims[depth - i - 1]))
             if self.include_batch_norm:
                 decoder.append(nn.BatchNorm1d(layer_dims[depth - i - 1]))
-            decoder.append(act_fn())
+            decoder.append(act_fn[depth - i - 1]())
         self.encoder = nn.Sequential(*encoder)
         self.decoder = nn.Sequential(*decoder)
