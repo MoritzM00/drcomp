384248f56fc67414738cea743bebf19f49bf091f
Moritz Mistol <moritz.mistol@gmail.com>
2022-12-09 16:40:17 +0100

    update scripts

--- scripts/train.py
+++ scripts/train.py
@@ -4,9 +4,11 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 
 
-@hydra.main(config_path="../conf", config_name="config")
+@hydra.main(version_base="1.3", config_path="../conf", config_name="config")
 def train(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
+    reducer = hydra.utils.instantiate(cfg.reducer)
+    print(reducer)
 
 
 if __name__ == "__main__":
