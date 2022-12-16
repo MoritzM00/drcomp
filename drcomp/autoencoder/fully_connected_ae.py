f38c453401b2a2266c80417af15758a4a15768e6
Moritz Mistol <moritz.mistol@gmail.com>
2022-12-16 12:02:02 +0100

    implement tied weights

--- drcomp/autoencoder/fully_connected_ae.py
+++ drcomp/autoencoder/fully_connected_ae.py
@@ -1,12 +1,33 @@
 """Fully Connected Autoencoder"""
+import logging
 from typing import Union
 
+import torch
 import torch.nn as nn
 
 from drcomp.autoencoder.base import AbstractAutoEncoder
 
+logger = logging.getLogger(__name__)
+
 
 class FullyConnectedAE(AbstractAutoEncoder):
+    """Fully Connected Autoencoder.
+
+    Parameters
+    ----------
+    input_size : int
+        Input dimension of the data.
+    intrinsic_dim : int
+        The intrinsic dimensionality determines the size of the bottleneck layer.
+    hidden_layer_dims : list[int], default=[]
+        List of hidden layer dimensions of the Encoder, by default []. Then a shallow autoencoder with one hidden layer is created.
+    act_fn : Union[object, list[object]], default=nn.Sigmoid
+        Activation function for the hidden layers, by default nn.Sigmoid. If a list is provided,
+        then each element of the list is used for the corresponding hidden layer.
+    include_batch_norm : bool, default=False
+        Whether to include batch normalization, by default False.
+    """
+
     def __init__(
         self,
         input_size: int,
@@ -14,6 +35,7 @@ class FullyConnectedAE(AbstractAutoEncoder):
         hidden_layer_dims: list[int] = [],
         act_fn: Union[object, list[object]] = nn.Sigmoid,
         include_batch_norm: bool = False,
+        tied_weights: bool = False,
     ):
         super().__init__(intrinsic_dim=intrinsic_dim)
         self.input_size = input_size
@@ -28,18 +50,31 @@ class FullyConnectedAE(AbstractAutoEncoder):
 
         # build encoder and decoder
         layer_dims = [input_size, *hidden_layer_dims, intrinsic_dim]
+        logger.debug(f"The encoder layer dimensions are {layer_dims}")
         encoder = nn.ModuleList()
         decoder = nn.ModuleList()
-        depth = len(layer_dims) - 1
+        depth = len(layer_dims) - 1  # this is the depth of the encoder
+        logger.debug(f"Depth of the encoder is {depth}")
         for i in range(depth):
-            encoder.append(nn.Linear(layer_dims[i], layer_dims[i + 1]))
+            encoder_layer = nn.Linear(layer_dims[i], layer_dims[i + 1])
+            encoder.append(encoder_layer)
             if include_batch_norm:
                 encoder.append(nn.BatchNorm1d(layer_dims[i + 1]))
             encoder.append(act_fn[i]())
 
-            decoder.append(nn.Linear(layer_dims[depth - i], layer_dims[depth - i - 1]))
+            decoder_layer = nn.Linear(layer_dims[depth - i], layer_dims[depth - i - 1])
+            if tied_weights:
+                # tie the weights by setting the decoder layer weight to be the transpose of the encoder layer weight.
+                logger.debug(
+                    f"Tying the weights of the encoder layer {i} and decoder layer {depth - i}."
+                )
+                layer_weight = torch.empty_like(encoder_layer.weight)
+                encoder_layer.weight = nn.Parameter(layer_weight)
+                decoder_layer.weight = nn.Parameter(layer_weight.t())
+            decoder.append(decoder_layer)
             if self.include_batch_norm:
                 decoder.append(nn.BatchNorm1d(layer_dims[depth - i - 1]))
             decoder.append(act_fn[depth - i - 1]())
+
         self.encoder = nn.Sequential(*encoder)
         self.decoder = nn.Sequential(*decoder)
