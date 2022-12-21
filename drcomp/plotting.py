cd1131d6a94a9e2e335a1a2cc735108b114e81ad
Moritz Mistol <moritz.mistol@gmail.com>
2022-12-21 14:26:49 +0100

    add plotting utils for metrics

--- drcomp/plotting.py
+++ drcomp/plotting.py
@@ -1,11 +1,40 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import umap
+from matplotlib.ticker import IndexLocator
 
 from drcomp import DimensionalityReducer
 
 
-def plot_trustworthiness_continuity():
-    pass
+def plot_trustworthiness_continuity(t, c, ax=None):
+    if len(t) != len(c):
+        raise ValueError(
+            "Trustworthiness and Continuity arrays must have the same length."
+        )
+    if ax is None:
+        ax = plt.axes()
+    x = np.arange(1, len(t) + 1)
+    ax.plot(x, t, label="Trustworthiness")
+    ax.plot(x, c, label="Continuity")
+    ax.set_xlabel("Number of neighbors")
+    ax.set_xlim(0, len(t) + 2)
+    ax.set_ylim(0, 1)
+    ax.xaxis.set_major_locator(IndexLocator(10, offset=-1))
+    ax.legend()
+    return ax
+
+
+def plot_lcmc(lcmc, ax=None):
+    if ax is None:
+        ax = plt.axes()
+    x = np.arange(1, len(lcmc) + 1)
+    ax.plot(x, lcmc)
+    ax.set_xlabel("Number of neighbors")
+    ax.set_ylabel("Local Continuity Meta Criterion")
+    ax.set_ylim(0, 1)
+    ax.set_xlim(0, len(lcmc) + 2)
+    ax.xaxis.set_major_locator(IndexLocator(10, offset=-1))
+    return ax
 
 
 def visualize_2D_latent_space(
